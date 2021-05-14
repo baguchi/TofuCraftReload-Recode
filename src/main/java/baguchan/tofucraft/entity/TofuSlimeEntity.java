732d4d71c0061545425b79587ad05b0fc87923ac
baguchan <shurf012@gmail.com>
2021-05-14 17:56:12 +0900

    fixed tofuslime spawn

--- src/main/java/baguchan/tofucraft/entity/TofuSlimeEntity.java
+++ src/main/java/baguchan/tofucraft/entity/TofuSlimeEntity.java
@@ -3,7 +3,6 @@ package baguchan.tofucraft.entity;
 import baguchan.tofucraft.registry.TofuItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.monster.SlimeEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.IParticleData;
@@ -12,6 +11,7 @@ import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.IServerWorld;
+import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -21,15 +21,25 @@ public class TofuSlimeEntity extends SlimeEntity {
 		super(p_i48552_1_, p_i48552_2_);
 	}
 
-	public static boolean checkTofuSlimeSpawnRules(EntityType<TofuSlimeEntity> p_223366_0_, IServerWorld p_223366_1_, SpawnReason p_223366_2_, BlockPos p_223366_3_, Random p_223366_4_) {
+	public static boolean checkTofuSlimeSpawnRules(EntityType<? extends TofuSlimeEntity> p_223366_0_, IServerWorld p_223366_1_, SpawnReason p_223366_2_, BlockPos p_223366_3_, Random p_223366_4_) {
 		if (p_223366_1_.getDifficulty() != Difficulty.PEACEFUL) {
 			if (!(p_223366_1_ instanceof net.minecraft.world.ISeedReader))
 				return false;
 			if (p_223366_4_.nextInt(10) == 0)
-				return MonsterEntity.checkMobSpawnRules(p_223366_0_, p_223366_1_, p_223366_2_, p_223366_3_, p_223366_4_);
+				return isDarkEnoughToSpawn(p_223366_1_, p_223366_3_, p_223366_4_);
 		}
 		return false;
 	}
+
+	public static boolean isDarkEnoughToSpawn(IServerWorld p_223323_0_, BlockPos p_223323_1_, Random p_223323_2_) {
+		if (p_223323_0_.getBrightness(LightType.SKY, p_223323_1_) > p_223323_2_.nextInt(32)) {
+			return false;
+		} else {
+			int i = p_223323_0_.getLevel().isThundering() ? p_223323_0_.getMaxLocalRawBrightness(p_223323_1_, 10) : p_223323_0_.getMaxLocalRawBrightness(p_223323_1_);
+			return i <= p_223323_2_.nextInt(8);
+		}
+	}
+
 
 	@Override
 	protected IParticleData getParticleType() {
