11045abf4677f682c005619c84c7bcb371c4c99d
baguchan <shurf012@gmail.com>
2021-09-23 07:51:43 +0900

    custom seed supported!

---
+++ src/main/java/baguchan/tofucraft/mixin/WorldGenSettingsMixin.java
@@ -0,0 +1,18 @@
+package baguchan.tofucraft.mixin;
+
+import baguchan.tofucraft.world.SeedHolder;
+import net.minecraft.core.MappedRegistry;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.WorldGenSettings;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldGenSettings.class)
+public class WorldGenSettingsMixin {
+	@Inject(method = {"<init>(JZZLnet/minecraft/core/MappedRegistry;Ljava/util/Optional;)V"}, at = {@At("RETURN")})
+	private void getSeedFromConstructor(long seed, boolean p_64615_, boolean p_64616_, MappedRegistry<LevelStem> p_64617_, java.util.Optional<String> p_64618_, CallbackInfo ci) {
+		SeedHolder.putInSeed(seed);
+	}
+}
--- src/main/java/baguchan/tofucraft/registry/TofuSounds.java
+++ src/main/java/baguchan/tofucraft/registry/TofuSounds.java
@@ -10,35 +10,25 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 @EventBusSubscriber(modid = TofuCraftReload.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class TofuSounds {
 	public static final SoundEvent SOFT_BGM = createEvent("music.soft");
-
 	public static final SoundEvent MILKY_EARTH_BGM = createEvent("music.milky_earth");
-
 	public static final SoundEvent ROUGH_GROUND_BGM = createEvent("music.rough_ground");
-
 	public static final SoundEvent TOFU_ROAD_BGM = createEvent("music.tofu_road");
-
 	public static final SoundEvent GREEN_BRANCH_BGM = createEvent("music.green_branch");
-
 	public static final SoundEvent TOFU_DUNGEON_BGM = createEvent("music.tofu_dungeon");
 
 	public static final SoundEvent TOFUBUGLE = createEvent("tofubugle");
 
 	public static final SoundEvent TOFUNIAN_YES = createEvent("mob.tofunian.yes");
-
 	public static final SoundEvent TOFUNIAN_NO = createEvent("mob.tofunian.no");
 
 	public static final SoundEvent TOFUNIAN_AMBIENT = createEvent("mob.tofunian.ambient");
-
 	public static final SoundEvent TOFUNIAN_DEATH = createEvent("mob.tofunian.death");
 
 	public static final SoundEvent ZOMBIE_TOFUNIAN_IDLE = createEvent("mob.zombie_tofunian.idle");
-
 	public static final SoundEvent ZOMBIE_TOFUNIAN_HURT = createEvent("mob.zombie_tofunian.hurt");
-
 	public static final SoundEvent ZOMBIE_TOFUNIAN_DEATH = createEvent("mob.zombie_tofunian.death");
 
 	public static final SoundEvent SOUL_BREATH = createEvent("block.soybean_soul.soul_breath");
-
 	public static final SoundEvent SOYBEAN_CRACK = createEvent("item.soybean.crack");
 
 	private static SoundEvent createEvent(String sound) {
