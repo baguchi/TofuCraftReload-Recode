3454b4a71ee8f4767927786dbb3d35d358d3ea87
baguchan <shurf012@gmail.com>
2021-12-08 15:17:47 +0900

    fixed soybean generation

--- src/main/java/baguchan/tofucraft/world/placement/ModNetherPlacements.java
+++ src/main/java/baguchan/tofucraft/world/placement/ModNetherPlacements.java
@@ -7,7 +7,7 @@ import net.minecraft.world.level.levelgen.placement.*;
 
 public class ModNetherPlacements {
 
-	public static final PlacementModifier NETHER_SOYBEAN_HEIGHT = HeightRangePlacement.triangle(VerticalAnchor.absolute(32), VerticalAnchor.belowTop(-20));
+	public static final PlacementModifier NETHER_SOYBEAN_HEIGHT = HeightRangePlacement.uniform(VerticalAnchor.absolute(32), VerticalAnchor.belowTop(-20));
 
 	public static final PlacedFeature PATCH_NETHER_SOYBEAN_NORMAL = PlacementUtils.register("tofucraft:patch_nether_soybean", ModNetherFeatures.NETHER_SOYBEAN.placed(RarityFilter.onAverageOnceEvery(2), InSquarePlacement.spread(), NETHER_SOYBEAN_HEIGHT, BiomeFilter.biome()));
 	public static final PlacedFeature PATCH_NETHER_SOYBEAN_CRIMSON = PlacementUtils.register("tofucraft:patch_nether_soybean_crimson", ModNetherFeatures.NETHER_SOYBEAN.placed(RarityFilter.onAverageOnceEvery(1), InSquarePlacement.spread(), NETHER_SOYBEAN_HEIGHT, BiomeFilter.biome()));
