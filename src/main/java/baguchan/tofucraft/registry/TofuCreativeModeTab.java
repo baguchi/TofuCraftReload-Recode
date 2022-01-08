0fb80575ad9afe249200b898245980831366850c
baguchan <shurf012@gmail.com>
2022-01-08 15:58:41 +0900

    fixed mistake and add compat page

--- src/main/java/baguchan/tofucraft/registry/TofuCreativeModeTab.java
+++ src/main/java/baguchan/tofucraft/registry/TofuCreativeModeTab.java
@@ -14,7 +14,7 @@ public class TofuCreativeModeTab {
 	public static final CreativeModeTab TOFU_DELIGHT = new CreativeModeTab("tofu_delight") {
 		@Override
 		public ItemStack makeIcon() {
-			return new ItemStack(TofuItems.TOFUMOMEN);
+			return new ItemStack(TofuItems.EDAMAME_RICE);
 		}
 	};
 }
