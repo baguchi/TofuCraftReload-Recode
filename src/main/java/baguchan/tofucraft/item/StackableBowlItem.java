faed1929758a7f052aaa4f9409add51d900a897f
baguchan <shurf012@gmail.com>
2022-01-08 16:12:00 +0900

    oops again

--- src/main/java/baguchan/tofucraft/item/StackableBowlItem.java
+++ src/main/java/baguchan/tofucraft/item/StackableBowlItem.java
@@ -3,11 +3,12 @@ package baguchan.tofucraft.item;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
 public class StackableBowlItem extends Item {
@@ -47,11 +48,6 @@ public class StackableBowlItem extends Item {
 	@Override
 	public UseAnim getUseAnimation(ItemStack p_41452_) {
 		return UseAnim.EAT;
-	}
-
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level p_41432_, Player p_41433_, InteractionHand p_41434_) {
-		return ItemUtils.startUsingInstantly(p_41432_, p_41433_, p_41434_);
 	}
 }
 
