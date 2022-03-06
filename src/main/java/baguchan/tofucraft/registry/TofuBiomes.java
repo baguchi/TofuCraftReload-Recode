c727b71708ac217aec7b5cc23f153b63818fb598
baguchan <shurf012@gmail.com>
2022-03-06 10:04:47 +0900

    tofu world is done!

--- src/main/java/baguchan/tofucraft/registry/TofuBiomes.java
+++ src/main/java/baguchan/tofucraft/registry/TofuBiomes.java
@@ -4,6 +4,7 @@ import baguchan.tofucraft.TofuCraftReload;
 import baguchan.tofucraft.world.biome.TofuBiomeBuilder;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.biome.OverworldBiomes;
 import net.minecraft.resources.ResourceKey;
@@ -13,8 +14,6 @@ import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-
-import java.util.function.Supplier;
 
 public class TofuBiomes {
 	public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, TofuCraftReload.MODID);
@@ -33,17 +32,12 @@ public class TofuBiomes {
 
 
 	public static final MultiNoiseBiomeSource.Preset TOFU_WORLD = new MultiNoiseBiomeSource.Preset(new ResourceLocation(TofuCraftReload.MODID, "tofu_world"), (p_187108_) -> {
-		ImmutableList.Builder<Pair<Climate.ParameterPoint, Supplier<Biome>>> builder = ImmutableList.builder();
-		(new TofuBiomeBuilder()).addBiomes((p_187098_) -> {
-			builder.add(p_187098_.mapSecond((p_187103_) -> {
-				return () -> {
-					return p_187108_.getOrThrow(p_187103_);
-				};
-			}));
+		ImmutableList.Builder<Pair<Climate.ParameterPoint, Holder<Biome>>> builder = ImmutableList.builder();
+		(new TofuBiomeBuilder()).addBiomes((p_204279_) -> {
+			builder.add(p_204279_.mapSecond(p_187108_::getOrCreateHolder));
 		});
-		return new Climate.ParameterList(builder.build());
+		return new Climate.ParameterList<>(builder.build());
 	});
-
 	public static void init() {
 
 	}
