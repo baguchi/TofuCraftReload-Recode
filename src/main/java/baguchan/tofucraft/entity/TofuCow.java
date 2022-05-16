9e428d64ce10d2c87289c4be9484bc2248329652
baguchan <shurf012@gmail.com>
2022-05-16 21:59:49 +0900

    fixed tofu cow cannot interest leek and fixed weired tofu cow baby

--- src/main/java/baguchan/tofucraft/entity/TofuCow.java
+++ src/main/java/baguchan/tofucraft/entity/TofuCow.java
@@ -1,22 +1,27 @@
 package baguchan.tofucraft.entity;
 
+import baguchan.tofucraft.registry.TofuEntityTypes;
 import baguchan.tofucraft.registry.TofuItems;
 import baguchan.tofucraft.registry.TofuTags;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 
@@ -25,6 +30,17 @@ import java.util.Random;
 public class TofuCow extends Cow {
 	public TofuCow(EntityType<? extends Cow> p_28285_, Level p_28286_) {
 		super(p_28285_, p_28286_);
+	}
+
+	protected void registerGoals() {
+		this.goalSelector.addGoal(0, new FloatGoal(this));
+		this.goalSelector.addGoal(1, new PanicGoal(this, 2.0D));
+		this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
+		this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(TofuItems.LEEK.get()), false));
+		this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25D));
+		this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+		this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
+		this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
@@ -45,6 +61,11 @@ public class TofuCow extends Cow {
 		} else {
 			return super.mobInteract(p_28298_, p_28299_);
 		}
+	}
+
+	@Override
+	public TofuCow getBreedOffspring(ServerLevel p_148890_, AgeableMob p_148891_) {
+		return TofuEntityTypes.TOFUCOW.get().create(p_148890_);
 	}
 
 	public boolean isFood(ItemStack p_27600_) {
