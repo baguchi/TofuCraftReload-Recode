b2b01f3dce225edadefc3f4e68b1952f62c7aba1
baguchan <shurf012@gmail.com>
2022-06-15 18:32:38 +0900

    oops

--- src/main/java/baguchan/tofucraft/registry/TofuFluidTypes.java
+++ src/main/java/baguchan/tofucraft/registry/TofuFluidTypes.java
@@ -11,7 +11,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class TofuFluidTypes {
-	public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(ForgeRegistries.FLUID_TYPES.get(), TofuCraftReload.MODID);
+	public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, TofuCraftReload.MODID);
 
 	public static final RegistryObject<FluidType> SOYMILK = FLUID_TYPES.register("soymilk", () -> new SoymilkFluidType(FluidType.Properties.create().canExtinguish(true).fallDistanceModifier(0F).canHydrate(true).supportsBoating(true)));
 	public static final RegistryObject<FluidType> SOYMILK_HELL = FLUID_TYPES.register("soymilk_hell", () -> new SoymilkHellFluidType(FluidType.Properties.create().canExtinguish(true).fallDistanceModifier(0F).canHydrate(true).supportsBoating(true)));
