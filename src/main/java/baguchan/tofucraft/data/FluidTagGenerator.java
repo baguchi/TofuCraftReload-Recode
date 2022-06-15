0954016d122f03f33899b09080f4dab860128c70
baguchan <shurf012@gmail.com>
2022-06-15 18:14:59 +0900

    no more fluid frozen

--- src/main/java/baguchan/tofucraft/data/FluidTagGenerator.java
+++ src/main/java/baguchan/tofucraft/data/FluidTagGenerator.java
@@ -1,10 +1,8 @@
 package baguchan.tofucraft.data;
 
 import baguchan.tofucraft.TofuCraftReload;
-import baguchan.tofucraft.registry.TofuFluids;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.FluidTagsProvider;
-import net.minecraft.tags.FluidTags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class FluidTagGenerator extends FluidTagsProvider {
@@ -15,9 +13,5 @@ public class FluidTagGenerator extends FluidTagsProvider {
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void addTags() {
-		tag(FluidTags.WATER).add(TofuFluids.SOYMILK.get(), TofuFluids.SOYMILK_FLOW.get()
-				, TofuFluids.SOYMILK_HELL.get(), TofuFluids.SOYMILK_HELL_FLOW.get()
-				, TofuFluids.SOYMILK_SOUL.get(), TofuFluids.SOYMILK_SOUL_FLOW.get()
-				, TofuFluids.BITTERN.get(), TofuFluids.BITTERN_FLOW.get());
 	}
 }
