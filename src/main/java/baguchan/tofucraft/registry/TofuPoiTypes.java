2bb5b162ae5b14908e787c0a4445630978b07bac
baguchan <shurf012@gmail.com>
2022-06-23 18:34:14 +0900

    fixed poitypes

--- src/main/java/baguchan/tofucraft/registry/TofuPoiTypes.java
+++ src/main/java/baguchan/tofucraft/registry/TofuPoiTypes.java
@@ -1,10 +1,13 @@
 package baguchan.tofucraft.registry;
 
 import baguchan.tofucraft.TofuCraftReload;
+import com.google.common.collect.ImmutableSet;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -19,11 +22,15 @@ public class TofuPoiTypes {
 	public static final ResourceKey<PoiType> MORIJIO = createKey("morijio");
 
 	// CREATE TOFU_CRAFTSMAN WORKSTATION AND PROFESSION
-	public static final RegistryObject<PoiType> TOFU_CRAFTSMAN_POI = POI_TYPES.register("tofu_craftsman", () -> new PoiType(Set.of(TofuBlocks.SALT_FURNACE.get().defaultBlockState()), 1, 1));
+	public static final RegistryObject<PoiType> TOFU_CRAFTSMAN_POI = POI_TYPES.register("tofu_craftsman", () -> new PoiType(getBlockStates(TofuBlocks.SALT_FURNACE.get()), 1, 1));
 
-	public static final RegistryObject<PoiType> MORIJIO_POI = POI_TYPES.register("morijio", () -> new PoiType(Set.of(TofuBlocks.MORIJIO.get().defaultBlockState()), 1, 1));
+	public static final RegistryObject<PoiType> MORIJIO_POI = POI_TYPES.register("morijio", () -> new PoiType(getBlockStates(TofuBlocks.MORIJIO.get()), 1, 1));
 
 	private static ResourceKey<PoiType> createKey(String p_218091_) {
 		return ResourceKey.create(Registry.POINT_OF_INTEREST_TYPE_REGISTRY, new ResourceLocation(TofuCraftReload.MODID, p_218091_));
+	}
+
+	private static Set<BlockState> getBlockStates(Block p_218074_) {
+		return ImmutableSet.copyOf(p_218074_.getStateDefinition().getPossibleStates());
 	}
 }
