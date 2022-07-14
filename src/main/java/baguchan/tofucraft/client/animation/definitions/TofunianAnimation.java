b14df1f56dc6239b4379e355566239824263dd60
baguchan <shurf012@gmail.com>
2022-07-15 08:47:17 +0900

    meme :tm:

--- src/main/java/baguchan/tofucraft/client/animation/definitions/TofunianAnimation.java
+++ src/main/java/baguchan/tofucraft/client/animation/definitions/TofunianAnimation.java
@@ -15,4 +15,19 @@ public class TofunianAnimation {
 					, new Keyframe(1.0F, KeyframeAnimations.degreeVec(32.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
 					, new Keyframe(1.25F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)))
 			.build();
+
+	public static final AnimationDefinition TOFUNIAN_DAB = AnimationDefinition.Builder.withLength(1.25F)
+			.addAnimation("right_arm", new AnimationChannel(AnimationChannel.Targets.ROTATION
+					, new Keyframe(0.0F, KeyframeAnimations.degreeVec(-115.0F, -115.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(1.0F, KeyframeAnimations.degreeVec(-115.0F, -115.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(1.25F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)))
+			.addAnimation("left_arm", new AnimationChannel(AnimationChannel.Targets.ROTATION
+					, new Keyframe(0.0F, KeyframeAnimations.degreeVec(-80.0F, -55.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(1.0F, KeyframeAnimations.degreeVec(-80.0F, -55.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(1.25F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)))
+			.addAnimation("head", new AnimationChannel(AnimationChannel.Targets.ROTATION
+					, new Keyframe(0.0F, KeyframeAnimations.degreeVec(50.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(1.0F, KeyframeAnimations.degreeVec(50.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(1.25F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)))
+			.build();
 }
