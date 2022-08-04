8241aa2d40fa27117f9caf2793b88bba451c3761
baguchan <shurf012@gmail.com>
2022-08-04 22:34:23 +0900

    add easter egg

--- src/main/java/baguchan/tofucraft/client/render/layer/TofunianClothLayer.java
+++ src/main/java/baguchan/tofucraft/client/render/layer/TofunianClothLayer.java
@@ -1,8 +1,10 @@
 package baguchan.tofucraft.client.render.layer;
 
+import baguchan.tofucraft.TofuCraftReload;
 import baguchan.tofucraft.client.model.TofunianModel;
 import baguchan.tofucraft.entity.Tofunian;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
@@ -12,6 +14,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class TofunianClothLayer extends RenderLayer<Tofunian, TofunianModel<Tofunian>> {
+	public static final ResourceLocation BAGU_LOCATION = new ResourceLocation(TofuCraftReload.MODID, "textures/entity/tofunian/bagu_chan.png");
+
 	public TofunianClothLayer(RenderLayerParent<Tofunian, TofunianModel<Tofunian>> tofunianRender) {
 		super(tofunianRender);
 	}
@@ -24,6 +28,11 @@ public class TofunianClothLayer extends RenderLayer<Tofunian, TofunianModel<Tofu
 
 	@Override
 	public ResourceLocation getTextureLocation(Tofunian entity) {
+		String s = ChatFormatting.stripFormatting(entity.getName().getString());
+		if (s != null && "bagu_chan".equals(s)) {
+			return BAGU_LOCATION;
+		}
+
 		String cloth = "";
 		if (entity.getTofunianType() != Tofunian.TofunianType.NORMAL)
 			cloth = entity.getTofunianType().name().toLowerCase();
