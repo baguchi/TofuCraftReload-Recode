a2c20c5f6ac065139e8a87df3469365dc0346e89
baguchan <shurf012@gmail.com>
2022-08-11 06:26:15 +0900

    more useful interactable

--- src/main/java/baguchan/tofucraft/entity/ItemBasedInteractable.java
+++ src/main/java/baguchan/tofucraft/entity/ItemBasedInteractable.java
@@ -4,12 +4,11 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.util.RandomSource;
 
-public class ItemBasedInteractable {
-	private final SynchedEntityData entityData;
-	private final EntityDataAccessor<Integer> healTimeAccessor;
+public abstract class ItemBasedInteractable {
+	protected final SynchedEntityData entityData;
+	protected final EntityDataAccessor<Integer> healTimeAccessor;
 	public boolean on = false;
-	public int healTime;
-	public int healTimeTotal;
+
 
 	public ItemBasedInteractable(SynchedEntityData entityData, EntityDataAccessor<Integer> healTimeAccessor) {
 		this.entityData = entityData;
@@ -18,21 +17,9 @@ public class ItemBasedInteractable {
 
 	public void onSynced() {
 		this.on = true;
-		this.healTime = 0;
-		this.healTimeTotal = this.entityData.get(this.healTimeAccessor);
 	}
 
-	public boolean active(RandomSource p_217033_) {
-		if (this.on) {
-			return false;
-		} else {
-			this.on = true;
-			this.healTime = 0;
-			this.healTimeTotal = p_217033_.nextInt(100) + 100;
-			this.entityData.set(this.healTimeAccessor, this.healTimeTotal);
-			return true;
-		}
-	}
+	public abstract boolean active(RandomSource p_217033_);
 
 	public void deActive() {
 		this.on = false;
