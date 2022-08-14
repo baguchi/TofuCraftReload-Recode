179c21644d3ee8605d2b392e83b5eb40f7650e7e
baguchan <shurf012@gmail.com>
2022-08-14 15:29:53 +0900

    1.19-2.3.6.1

--- src/main/java/baguchan/tofucraft/fluidtype/SoymilkHellFluidType.java
+++ src/main/java/baguchan/tofucraft/fluidtype/SoymilkHellFluidType.java
@@ -59,7 +59,7 @@ public class SoymilkHellFluidType extends FluidType {
 				float brightness = LightTexture.getBrightness(mc.player.level.dimensionType(), mc.player.level.getMaxLocalRawBrightness(playerEyePos));
 				RenderSystem.enableBlend();
 				RenderSystem.defaultBlendFunc();
-				RenderSystem.setShaderColor(brightness, brightness, brightness, 0.75F);
+				RenderSystem.setShaderColor(brightness, brightness, brightness, 0.85F);
 				float uOffset = -mc.player.getYRot() / 64.0F;
 				float vOffset = mc.player.getXRot() / 64.0F;
 				Matrix4f pose = stack.last().pose();
