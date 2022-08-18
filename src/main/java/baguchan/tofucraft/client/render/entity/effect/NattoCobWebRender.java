1b8ca64b52e9b51afb2fb91cdaab1eec45febdf6
baguchan <shurf012@gmail.com>
2022-08-18 09:57:23 +0900

    fixed natto cobweb render

--- src/main/java/baguchan/tofucraft/client/render/entity/effect/NattoCobWebRender.java
+++ src/main/java/baguchan/tofucraft/client/render/entity/effect/NattoCobWebRender.java
@@ -2,14 +2,20 @@ package baguchan.tofucraft.client.render.entity.effect;
 
 import baguchan.tofucraft.TofuCraftReload;
 import baguchan.tofucraft.entity.effect.NattoCobWebEntity;
+import baguchan.tofucraft.registry.TofuItems;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Matrix3f;
-import com.mojang.math.Matrix4f;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -19,25 +25,31 @@ public class NattoCobWebRender extends EntityRenderer<NattoCobWebEntity> {
 	public static final ResourceLocation NATTOCOBWEB_LOCATION = new ResourceLocation(TofuCraftReload.MODID, "textures/item/natto_cobweb.png");
 	private static final float TEXTURE_WIDTH = 16;
 	private static final float TEXTURE_HEIGHT = 16;
+	private final ItemRenderer itemRenderer;
 
-	public NattoCobWebRender(EntityRendererProvider.Context p_174008_) {
-		super(p_174008_);
+	public NattoCobWebRender(EntityRendererProvider.Context context) {
+		super(context);
+
+		this.itemRenderer = context.getItemRenderer();
 	}
 
 	@Override
-	public ResourceLocation getTextureLocation(NattoCobWebEntity p_114482_) {
-		return NattoCobWebRender.NATTOCOBWEB_LOCATION;
+	public void render(NattoCobWebEntity entity, float yaw, float delta, PoseStack stack, MultiBufferSource buffer, int packedLightIn) {
+		stack.pushPose();
+		stack.scale(6.0F, 6.0F, 6.0F);
+		stack.translate(0.0F, 0.0F, -0.15F);
+		stack.mulPose(Vector3f.XP.rotationDegrees(90.0F));
+
+
+		ItemStack itemStack = new ItemStack(TofuItems.NATTO_COBWEB.get());
+		BakedModel bakedmodel = this.itemRenderer.getModel(itemStack, entity.level, (LivingEntity) null, entity.getId());
+
+		this.itemRenderer.render(itemStack, ItemTransforms.TransformType.GROUND, false, stack, buffer, packedLightIn, OverlayTexture.NO_OVERLAY, bakedmodel);
+		stack.popPose();
+		super.render(entity, yaw, delta, stack, buffer, packedLightIn);
 	}
 
-	public void render(NattoCobWebEntity entity, float yaw, float delta, PoseStack stack, int packedLightIn) {
-		drawCobWeb(entity, delta, stack, packedLightIn);
-	}
-
-	private void drawCobWeb(NattoCobWebEntity entity, float delta, PoseStack matrixStack, int packedLightIn) {
-
-	}
-
-	public void drawVertex(Matrix4f matrix, Matrix3f normals, VertexConsumer vertexBuilder, float offsetX, float offsetY, float offsetZ, float textureX, float textureY, float alpha, int packedLightIn) {
-		vertexBuilder.vertex(matrix, offsetX, offsetY, offsetZ).color(1, 1, 1, 1 * alpha).uv(textureX, textureY).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(packedLightIn).normal(normals, 0.0F, 1.0F, 0.0F).endVertex();
+	public ResourceLocation getTextureLocation(NattoCobWebEntity p_116083_) {
+		return TextureAtlas.LOCATION_BLOCKS;
 	}
 }
