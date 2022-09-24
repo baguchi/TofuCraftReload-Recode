99c599cbc36305441b0e2fc2d4b0f67a5f10d5bf
T-Niijima <rizastar@gmail.com>
2022-09-24 09:22:35 +0900

    Add rice for cow food

---
+++ src/main/java/baguchan/tofucraft/mixin/CowMixin.java
@@ -0,0 +1,47 @@
+package baguchan.tofucraft.mixin;
+
+import baguchan.tofucraft.registry.TofuItems;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.goal.BreedGoal;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.FollowParentGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.PanicGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Cow.class)
+public abstract class CowMixin extends Animal {
+
+	private static final Ingredient FOOD_ITEMS = Ingredient.of(Items.WHEAT, TofuItems.RICE.get());
+
+	protected CowMixin(EntityType<? extends Animal> p_27557_, Level p_27558_) {
+		super(p_27557_, p_27558_);
+	}
+
+	@Override
+	public boolean isFood(ItemStack p_27600_) {
+		return FOOD_ITEMS.test(p_27600_);
+	}
+
+	protected void registerGoals() {
+		this.goalSelector.addGoal(0, new FloatGoal(this));
+		this.goalSelector.addGoal(1, new PanicGoal(this, 2.0D));
+		this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
+		this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, FOOD_ITEMS, false));
+		this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25D));
+		this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+		this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
+		this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+	}
+
+}
