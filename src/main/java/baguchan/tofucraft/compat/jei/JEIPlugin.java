ee453882e71b87dcbe4fb258b30971801379eaa6
baguchan <shurf012@gmail.com>
2022-11-17 16:49:12 +0900

    add info for jei

--- src/main/java/baguchan/tofucraft/compat/jei/JEIPlugin.java
+++ src/main/java/baguchan/tofucraft/compat/jei/JEIPlugin.java
@@ -3,20 +3,25 @@ package baguchan.tofucraft.compat.jei;
 import baguchan.tofucraft.TofuCraftReload;
 import baguchan.tofucraft.recipe.BitternRecipe;
 import baguchan.tofucraft.recipe.HardenRecipe;
+import baguchan.tofucraft.registry.TofuBlocks;
 import baguchan.tofucraft.registry.TofuItems;
 import baguchan.tofucraft.registry.TofuRecipes;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.List;
 
@@ -45,6 +50,20 @@ public class JEIPlugin implements IModPlugin {
 
 	@Override
 	public void registerRecipes(IRecipeRegistration registration) {
+		addInfo(registration, TofuItems.BITTERN_BOTTLE.get());
+		addInfo(registration, TofuBlocks.KINUTOFU.get().asItem());
+		addInfo(registration, TofuBlocks.MOMENTOFU.get().asItem());
+		addInfo(registration, TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_APPLE.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_ANNIN.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_PUDDING.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_PUMPKIN.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_STRAWBERRY.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_FRUITS.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_TEA.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_HONEY.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_RAMUNE.get(), TofuItems.SOYMILK.get());
+		addInfo(registration, TofuItems.SOYMILK_SAKURA.get(), TofuItems.SOYMILK.get());
 		registration.addRecipes(HARDEN_JEI_TYPE, findRecipesByType(TofuRecipes.RECIPETYPE_HARDER));
 		registration.addRecipes(BITTERN_JEI_TYPE, findRecipesByType(TofuRecipes.RECIPETYPE_BITTERN));
 	}
@@ -53,6 +72,20 @@ public class JEIPlugin implements IModPlugin {
 	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
 		registration.addRecipeCatalyst(new ItemStack(Blocks.COBBLESTONE), HARDEN_JEI_TYPE);
 		registration.addRecipeCatalyst(new ItemStack(TofuItems.BITTERN_BOTTLE.get()), BITTERN_JEI_TYPE);
+	}
+
+	private static void addInfo(IRecipeRegistration registration, Item item) {
+		registration.addIngredientInfo(
+				new ItemStack(item),
+				VanillaTypes.ITEM_STACK,
+				Component.translatable(TofuCraftReload.MODID + "." + ForgeRegistries.ITEMS.getKey(item).getPath() + ".jei_desc"));
+	}
+
+	private static void addInfo(IRecipeRegistration registration, Item item, Item originalDescItem) {
+		registration.addIngredientInfo(
+				new ItemStack(item),
+				VanillaTypes.ITEM_STACK,
+				Component.translatable(TofuCraftReload.MODID + "." + ForgeRegistries.ITEMS.getKey(originalDescItem).getPath() + ".jei_desc"));
 	}
 
 	@Override
