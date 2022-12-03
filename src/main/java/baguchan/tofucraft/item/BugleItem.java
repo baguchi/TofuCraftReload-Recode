7e3f50ef847835b495c3eaef347a3b01614d2fa0
baguchan <shutux500@gmail.com>
2022-12-03 20:31:14 +0900

    when you cannot hasLineOfSight about spider. don't consume it

--- src/main/java/baguchan/tofucraft/item/BugleItem.java
+++ src/main/java/baguchan/tofucraft/item/BugleItem.java
@@ -4,6 +4,7 @@ import baguchan.tofucraft.entity.TofuSpider;
 import baguchan.tofucraft.registry.TofuSounds;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
@@ -11,8 +12,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.Collections;
 import java.util.List;
@@ -40,19 +44,38 @@ public class BugleItem extends Item {
 			if (p_41414_.getOffhandItem().is(Items.ECHO_SHARD)) {
 				if (p_41414_ instanceof Player) {
 					Player playerentity = (Player) p_41414_;
-					playerentity.getCooldowns().addCooldown(this, 600);
-					p_41414_.getOffhandItem().shrink(1);
-					List<TofuSpider> entities = p_41413_.getNearbyEntities(TofuSpider.class, TARGETING, p_41414_, p_41414_.getBoundingBox().inflate(18.0D));
+					List<TofuSpider> entities = p_41413_.getNearbyEntities(TofuSpider.class, TARGETING, p_41414_, p_41414_.getBoundingBox().inflate(6.0D));
 
 
 					if (!entities.isEmpty()) {
 						Collections.shuffle(entities);
-						entities.get(0).startConverting(300);
-						p_41413_.levelEvent(3007, entities.get(0).blockPosition(), 0);
+
+						if (hasLineOfSight(playerentity, entities.get(0))) {
+							playerentity.getCooldowns().addCooldown(this, 600);
+							p_41414_.getOffhandItem().shrink(1);
+
+							entities.get(0).startConverting(300);
+							p_41413_.levelEvent(3007, entities.get(0).blockPosition(), 0);
+						}
 					}
 				}
 			} else {
 				p_41414_.playSound(TofuSounds.TOFUBUGLE.get(), 3.0F, 1.0F);
+			}
+		}
+	}
+
+	//Prevent change in hasLineOfSight mixin
+	public static boolean hasLineOfSight(LivingEntity livingEntity, Entity p_147185_) {
+		if (p_147185_.level != livingEntity.level) {
+			return false;
+		} else {
+			Vec3 vec3 = new Vec3(livingEntity.getX(), livingEntity.getEyeY(), livingEntity.getZ());
+			Vec3 vec31 = new Vec3(p_147185_.getX(), p_147185_.getEyeY(), p_147185_.getZ());
+			if (vec31.distanceTo(vec3) > 128.0D) {
+				return false;
+			} else {
+				return livingEntity.level.clip(new ClipContext(vec3, vec31, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, livingEntity)).getType() == HitResult.Type.MISS;
 			}
 		}
 	}
