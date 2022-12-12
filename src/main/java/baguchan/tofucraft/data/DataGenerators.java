c625385ae8cef94755050b69acec17347ff1ebf0
baguchan <shutux500@gmail.com>
2022-12-12 09:07:34 +0900

    test

--- src/main/java/baguchan/tofucraft/data/DataGenerators.java
+++ src/main/java/baguchan/tofucraft/data/DataGenerators.java
@@ -5,6 +5,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,8 +16,9 @@ import java.util.concurrent.CompletableFuture;
 public class DataGenerators {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent event) {
-		DataGenerator gen = event.getGenerator();
-		PackOutput packOutput = gen.getPackOutput();
+		DataGenerator generator = event.getGenerator();
+		PackOutput packOutput = generator.getPackOutput();
+		ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 		event.getGenerator().addProvider(event.includeClient(), new BlockstateGenerator(event.getGenerator(), event.getExistingFileHelper()));
 		event.getGenerator().addProvider(event.includeClient(), new ItemModelGenerator(event.getGenerator(), event.getExistingFileHelper()));
@@ -24,10 +26,10 @@ public class DataGenerators {
 		event.getGenerator().addProvider(event.includeServer(), blocktags);
 		event.getGenerator().addProvider(event.includeServer(), new ItemTagGenerator(packOutput, lookupProvider, blocktags, event.getExistingFileHelper()));
 		event.getGenerator().addProvider(event.includeServer(), new EntityTagGenerator(packOutput, lookupProvider, event.getExistingFileHelper()));
-
 		event.getGenerator().addProvider(event.includeServer(), new FluidTagGenerator(packOutput, lookupProvider, event.getExistingFileHelper()));
 		event.getGenerator().addProvider(event.includeServer(), TofuLootTableProvider.create(packOutput));
 		event.getGenerator().addProvider(event.includeServer(), new CraftingGenerator(packOutput));
+
 		event.getGenerator().addProvider(event.includeServer(), new WorldGenerator(packOutput));
 	}
 }
