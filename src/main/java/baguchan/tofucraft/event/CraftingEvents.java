a799e6eeedd3472d985bd5479657df6240759080
baguchan <shutux500@gmail.com>
2023-02-21 11:07:46 +0900

    fixed AE2 conflict

--- src/main/java/baguchan/tofucraft/event/CraftingEvents.java
+++ src/main/java/baguchan/tofucraft/event/CraftingEvents.java
@@ -6,11 +6,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.Optional;
+import java.util.Set;
 
 public class CraftingEvents {
 
@@ -19,10 +21,13 @@ public class CraftingEvents {
 		Player player = event.getEntity();
 		ItemStack item = event.getCrafting();
 		Container craftMatrix = event.getInventory();
-		Optional<? extends Recipe> recipe = player.level.getRecipeManager().byKey(new ResourceLocation(TofuCraftReload.MODID, "bucket_soymilk_okara"));
-		if (recipe.isPresent() && recipe.stream().findFirst().get().matches(craftMatrix, player.level)) {
-			player.getInventory().add(new ItemStack(TofuItems.OKARA.get(), 1));
+		if (item.is(TofuItems.BUCKET_SOYMILK.get())) {
+			if (craftMatrix.hasAnyOf(Set.of(Items.BUCKET, TofuItems.FILTERCLOTH.get(), TofuItems.SEEDS_SOYBEANS.get()))) {
+				Optional<? extends Recipe> recipe = player.level.getRecipeManager().byKey(new ResourceLocation(TofuCraftReload.MODID, "bucket_soymilk_okara"));
+				if (recipe.isPresent() && recipe.stream().findFirst().get().matches(craftMatrix, player.level)) {
+					player.getInventory().add(new ItemStack(TofuItems.OKARA.get(), 1));
+				}
+			}
 		}
-
 	}
 }
