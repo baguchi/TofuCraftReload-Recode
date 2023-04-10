84808b8e5add6707c5f390f7eca3a47f8f41e92c
baguchan <shutux500@gmail.com>
2023-04-10 19:01:02 +0900

    1.19.4-4.1.0.2

--- src/main/java/baguchan/tofucraft/entity/AbstractTofunian.java
+++ src/main/java/baguchan/tofucraft/entity/AbstractTofunian.java
@@ -3,6 +3,7 @@ package baguchan.tofucraft.entity;
 import baguchan.tofucraft.registry.TofuSounds;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Pose;
@@ -12,14 +13,33 @@ import net.minecraft.world.level.Level;
 import javax.annotation.Nullable;
 
 public abstract class AbstractTofunian extends AbstractVillager {
+	protected int idleAnimationTimeout;
+	public final AnimationState idleAnimationState = new AnimationState();
+
 	public AbstractTofunian(EntityType<? extends AbstractTofunian> type, Level worldIn) {
 		super(type, worldIn);
 	}
 
+	@Override
 	public void tick() {
 		super.tick();
-		if (getUnhappyCounter() > 0)
+		if (getUnhappyCounter() > 0) {
 			setUnhappyCounter(getUnhappyCounter() - 1);
+		}
+		if (this.level.isClientSide()) {
+			this.setupAnimationStates();
+		}
+	}
+
+	public void setupAnimationStates() {
+		if (!this.isSleeping()) {
+			if (this.idleAnimationTimeout <= 0) {
+				this.idleAnimationTimeout = this.random.nextInt(40) + 80;
+				this.idleAnimationState.start(this.tickCount);
+			} else {
+				--this.idleAnimationTimeout;
+			}
+		}
 	}
 
 	@Nullable
