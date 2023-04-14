306fb477b9e956354032fbb0241d309327f46c57
baguchan <shutux500@gmail.com>
2023-04-14 16:05:50 +0900

    1.19.2-2.10.1.3

--- src/main/java/baguchan/tofucraft/data/ItemTagGenerator.java
+++ src/main/java/baguchan/tofucraft/data/ItemTagGenerator.java
@@ -31,5 +31,11 @@ public class ItemTagGenerator extends ItemTagsProvider {
 		tag(ItemTags.ARROWS).add(TofuItems.ZUNDA_ARROW.get());
 		tag(Tags.Items.SEEDS).add(TofuItems.SEEDS_CHILI.get(), TofuItems.SEEDS_SOYBEANS.get(), TofuItems.SEEDS_SOYBEANS_NETHER.get(), TofuItems.SEEDS_SOYBEANS_SOUL.get(), TofuItems.SEEDS_RICE.get());
 		tag(TofuTags.Items.RICE_BLOCK).add(TofuBlocks.RICE_BLOCK.get().asItem());
+		tag(TofuTags.Items.NATTO).add(TofuItems.NATTO.get());
+		tag(TofuTags.Items.MISO).add(TofuItems.MISO.get());
+		tag(TofuTags.Items.SOYSAUCE_SOYSAUCE).add(TofuItems.BOTTLE_SOYSAUSE.get());
+		tag(TofuTags.Items.SOYSAUCE).addTag(TofuTags.Items.SOYSAUCE_SOYSAUCE);
+		tag(TofuTags.Items.TOFU).add(TofuItems.TOFUKINU.get()).add(TofuItems.TOFUMOMEN.get());
+		tag(TofuTags.Items.TOFU_FRIED).add(TofuItems.TOFUFRIED.get());
 	}
 }
