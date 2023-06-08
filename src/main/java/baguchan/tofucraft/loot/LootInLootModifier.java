e83f01d25c2b4ef94a54766047067d5b24407087
baguchan <shutux500@gmail.com>
2023-06-09 07:20:18 +0900

    I port to 1.20 thanks I hate it

---
+++ src/main/java/baguchan/tofucraft/loot/LootInLootModifier.java
@@ -0,0 +1,45 @@
+package baguchan.tofucraft.loot;
+
+import com.google.common.base.Suppliers;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.common.loot.LootModifier;
+
+import javax.annotation.Nonnull;
+import java.util.function.Supplier;
+
+public class LootInLootModifier extends LootModifier {
+
+	public static final Supplier<Codec<LootInLootModifier>> CODEC = Suppliers.memoize(() ->
+			RecordCodecBuilder.create(inst -> codecStart(inst)
+					.and(ResourceLocation.CODEC.fieldOf("loot_table").forGetter((m) -> m.lootTable))
+					.apply(inst, LootInLootModifier::new)));
+
+	public final ResourceLocation lootTable;
+
+	protected LootInLootModifier(LootItemCondition[] conditionsIn, ResourceLocation lootTable) {
+		super(conditionsIn);
+		this.lootTable = lootTable;
+	}
+
+	@Nonnull
+	@Override
+	protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+		ObjectArrayList<ItemStack> objectarraylist = new ObjectArrayList<>();
+		objectarraylist = ForgeHooks.modifyLoot(this.lootTable, objectarraylist, context);
+		generatedLoot.addAll(objectarraylist);
+		return generatedLoot;
+	}
+
+	@Override
+	public Codec<? extends IGlobalLootModifier> codec() {
+		return CODEC.get();
+	}
+}
