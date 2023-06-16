86f42dc6e9b1c517cd3aeae1671832bb8deee261
baguchan <shutux500@gmail.com>
2023-06-16 16:25:34 +0900

    fluid mechanic changed and tofu upgrade item changed

--- src/main/java/baguchan/tofucraft/registry/ModInteractionInformations.java
+++ src/main/java/baguchan/tofucraft/registry/ModInteractionInformations.java
@@ -7,13 +7,25 @@ public class ModInteractionInformations {
 	public static void init() {
 		FluidInteractionRegistry.addInteraction(ForgeMod.LAVA_TYPE.get(), new FluidInteractionRegistry.InteractionInformation(
 				TofuFluidTypes.SOYMILK.get(),
-				fluidState -> TofuBlocks.TOFU_TERRAIN.get().defaultBlockState()
+				fluidState -> TofuBlocks.TOFUSLATE.get().defaultBlockState()
 		));
 		FluidInteractionRegistry.addInteraction(ForgeMod.LAVA_TYPE.get(), new FluidInteractionRegistry.InteractionInformation(
 				TofuFluidTypes.SOYMILK_HELL.get(),
-				fluidState -> TofuBlocks.TOFU_TERRAIN.get().defaultBlockState()
+				fluidState -> TofuBlocks.TOFUSLATE.get().defaultBlockState()
 		));
 		FluidInteractionRegistry.addInteraction(ForgeMod.LAVA_TYPE.get(), new FluidInteractionRegistry.InteractionInformation(
+				TofuFluidTypes.SOYMILK_SOUL.get(),
+				fluidState -> TofuBlocks.TOFUSLATE.get().defaultBlockState()
+		));
+		FluidInteractionRegistry.addInteraction(TofuFluidTypes.BITTERN.get(), new FluidInteractionRegistry.InteractionInformation(
+				TofuFluidTypes.SOYMILK.get(),
+				fluidState -> TofuBlocks.TOFU_TERRAIN.get().defaultBlockState()
+		));
+		FluidInteractionRegistry.addInteraction(TofuFluidTypes.BITTERN.get(), new FluidInteractionRegistry.InteractionInformation(
+				TofuFluidTypes.SOYMILK_HELL.get(),
+				fluidState -> TofuBlocks.TOFU_TERRAIN.get().defaultBlockState()
+		));
+		FluidInteractionRegistry.addInteraction(TofuFluidTypes.BITTERN.get(), new FluidInteractionRegistry.InteractionInformation(
 				TofuFluidTypes.SOYMILK_SOUL.get(),
 				fluidState -> TofuBlocks.TOFU_TERRAIN.get().defaultBlockState()
 		));
