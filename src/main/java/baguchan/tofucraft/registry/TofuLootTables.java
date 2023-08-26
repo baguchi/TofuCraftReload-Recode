b5ea718b5a0848ce971378da327c1df203e0e9b2
baguchan <shutux500@gmail.com>
2023-08-26 10:35:54 +0900

    soy fishing

---
+++ src/main/java/baguchan/tofucraft/registry/TofuLootTables.java
@@ -0,0 +1,8 @@
+package baguchan.tofucraft.registry;
+
+import baguchan.tofucraft.TofuCraftReload;
+import net.minecraft.resources.ResourceLocation;
+
+public class TofuLootTables {
+    public static final ResourceLocation TOFU_WORLD_FISHING_LOOT_TABLE = new ResourceLocation(TofuCraftReload.MODID, "gameplay/fishing/fish");
+}
