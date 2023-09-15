6d6e2efab08f62bfb3bfab0b05345488a1c518de
baguchan <shutux500@gmail.com>
2023-09-16 07:21:21 +0900

    add zundama block

--- src/main/java/baguchan/tofucraft/registry/TofuBlocks.java
+++ src/main/java/baguchan/tofucraft/registry/TofuBlocks.java
@@ -25,6 +25,7 @@ import baguchan.tofucraft.block.TofuTrapDoorBlock;
 import baguchan.tofucraft.block.TofuWallHangingSignBlock;
 import baguchan.tofucraft.block.TofuWallSignBlock;
 import baguchan.tofucraft.block.YubaBlock;
+import baguchan.tofucraft.block.ZundamaBlock;
 import baguchan.tofucraft.block.crop.ChiliCropsBlock;
 import baguchan.tofucraft.block.crop.LeekCropsBlock;
 import baguchan.tofucraft.block.crop.RiceCropsBlock;
@@ -392,7 +393,7 @@ public class TofuBlocks {
 	public static final RegistryObject<Block> RED_SOYCHEESE_CANDLE_TART = noItemRegister("red_candle_soycheese_candle_tart", () -> new CandleTofuCakeBlock(SOYCHEESE_TART, Blocks.RED_CANDLE, BlockBehaviour.Properties.copy(Blocks.CAKE)));
 	public static final RegistryObject<Block> BLACK_SOYCHEESE_CANDLE_TART = noItemRegister("black_candle_soycheese_candle_tart", () -> new CandleTofuCakeBlock(SOYCHEESE_TART, Blocks.BLACK_CANDLE, BlockBehaviour.Properties.copy(Blocks.CAKE)));
 
-	public static final RegistryObject<HoneyBlock> ZUNDAMA_BLOCK = register("zundama_block", () -> new HoneyBlock(BlockBehaviour.Properties.of().strength(0.4F).noOcclusion().lightLevel((blockState) -> 9).sound(SoundType.HONEY_BLOCK)));
+	public static final RegistryObject<HoneyBlock> ZUNDAMA_BLOCK = register("zundama_block", () -> new ZundamaBlock(BlockBehaviour.Properties.of().strength(0.4F).noOcclusion().lightLevel((blockState) -> 9).sound(SoundType.HONEY_BLOCK)));
 
 	public static final RegistryObject<SoymilkCauldronBlock> SOYMILK_CAULDRON = noItemRegister("soymilk_cauldron", () -> new SoymilkCauldronBlock(BlockBehaviour.Properties.of().strength(2.0F).noOcclusion().requiresCorrectToolForDrops().randomTicks().sound(SoundType.METAL), TofuItems.BUCKET_SOYMILK, TofuItems.SOY_CHEESE));
 	public static final RegistryObject<SoymilkCauldronBlock> SOYMILK_NETHER_CAULDRON = noItemRegister("soymilk_nether_cauldron", () -> new SoymilkCauldronBlock(BlockBehaviour.Properties.of().strength(2.0F).noOcclusion().requiresCorrectToolForDrops().randomTicks().sound(SoundType.METAL), TofuItems.BUCKET_SOYMILK_NETHER, TofuItems.SOY_NETHER_CHEESE));
