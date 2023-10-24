019810a573918a85d1d347a5e436f50cab8631d9
baguchan <shutux500@gmail.com>
2023-10-24 19:10:00 +0900

    1.20.1-5.7.1.0

--- src/main/java/baguchan/tofucraft/effect/SoyHealthyEffect.java
+++ src/main/java/baguchan/tofucraft/effect/SoyHealthyEffect.java
@@ -18,7 +18,7 @@ public class SoyHealthyEffect extends MobEffect {
 	}
 
 	@Override
-	public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+	public boolean isDurationEffectTick(int duration, int amplifier) {
 		return true;
 	}
 }
