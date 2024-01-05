b5b86dd0434a45f0dbb2d49c027c73d0e34f3c4a
baguchan <shutux500@gmail.com>
2024-01-05 09:11:12 +0900

    noise gen changed

--- src/main/java/baguchan/tofucraft/data/resources/TofuNoiseBuilder.java
+++ src/main/java/baguchan/tofucraft/data/resources/TofuNoiseBuilder.java
@@ -60,7 +60,7 @@ public class TofuNoiseBuilder {
 		DensityFunction densityfunction2 = DensityFunctions.noise(p_256236_.getOrThrow(Noises.CAVE_LAYER), 8.0D);
 		DensityFunction densityfunction3 = DensityFunctions.mul(DensityFunctions.constant(4.0D), densityfunction2.square());
 		DensityFunction densityfunction4 = DensityFunctions.noise(p_256236_.getOrThrow(Noises.CAVE_CHEESE), 0.6666666666666666D);
-		DensityFunction densityfunction5 = DensityFunctions.add(DensityFunctions.add(DensityFunctions.constant(0.5D), densityfunction4).clamp(-1.0D, 1.0D), DensityFunctions.add(DensityFunctions.constant(-0.2D), DensityFunctions.mul(DensityFunctions.constant(-0.64D), p_256658_)).clamp(-0.5D, 0.5D));
+		DensityFunction densityfunction5 = DensityFunctions.add(DensityFunctions.add(DensityFunctions.constant(0.5D), densityfunction4).clamp(-1.0D, 1.0D), DensityFunctions.add(DensityFunctions.constant(-0.2D), DensityFunctions.mul(DensityFunctions.constant(-0.64D), p_256658_)).clamp(-0.25D, 0.5D));
 		DensityFunction densityfunction6 = DensityFunctions.add(densityfunction3, densityfunction5);
 		DensityFunction densityfunction7 = DensityFunctions.min(DensityFunctions.min(densityfunction6, getFunction(p_256548_, ENTRANCES)), DensityFunctions.add(densityfunction, densityfunction1));
 		DensityFunction densityfunction8 = getFunction(p_256548_, PILLARS);
