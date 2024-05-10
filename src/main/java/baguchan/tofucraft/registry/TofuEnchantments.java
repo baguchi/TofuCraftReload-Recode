bb3d5a921d4b389df150996087495ce301b33662
baguchan <shutux500@gmail.com>
2024-05-10 19:05:12 +0900

    fixed batch enchantment

--- src/main/java/baguchan/tofucraft/registry/TofuEnchantments.java
+++ src/main/java/baguchan/tofucraft/registry/TofuEnchantments.java
@@ -18,7 +18,7 @@ public class TofuEnchantments {
 	public static final DeferredRegister<Enchantment> ENCHANTMENT = DeferredRegister.create(Registries.ENCHANTMENT, TofuCraftReload.MODID);
 
 
-	public static final Supplier<Enchantment> BATCH = ENCHANTMENT.register("batch", () -> new BatchEnchantment(Enchantment.definition(TofuTags.Items.TOFU_DIAMOND_MINEABLE_ENCHANTABLE, 1, 2, Enchantment.dynamicCost(20, 15), Enchantment.dynamicCost(50, 15), 4, EquipmentSlot.MAINHAND)));
+	public static final Supplier<Enchantment> BATCH = ENCHANTMENT.register("batch", () -> new BatchEnchantment(Enchantment.definition(TofuTags.Items.TOFU_DIAMOND_MINEABLE_ENCHANTABLE, 1, 1, Enchantment.dynamicCost(20, 15), Enchantment.dynamicCost(50, 15), 4, EquipmentSlot.MAINHAND)));
 	public static final Supplier<Enchantment> DRAIN = ENCHANTMENT.register("drain", () -> new DrainEnchantment(Enchantment.definition(TofuTags.Items.TOFU_DIAMOND_SWORD_ENCHANTABLE, 1, 2, Enchantment.dynamicCost(20, 15), Enchantment.dynamicCost(50, 15), 3, EquipmentSlot.MAINHAND)));
 	public static final Supplier<Enchantment> EFFECT_PROTECTION = ENCHANTMENT.register("effect_protection", () -> new EffectProtectionEnchantment(Enchantment.definition(TofuTags.Items.TOFU_DIAMOND_ARMOR_ENCHANTABLE, 1, 1, Enchantment.dynamicCost(20, 10), Enchantment.dynamicCost(50, 10), 4, ARMOR_SLOTS)));
 
