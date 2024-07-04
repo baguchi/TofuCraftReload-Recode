8f4a9569c850956f5ac7759ee25108e40d563825
baguchan <shutux500@gmail.com>
2024-07-05 08:29:53 +0900

    rebalance fukumame

--- src/main/java/baguchan/tofucraft/registry/TofuEnchantments.java
+++ src/main/java/baguchan/tofucraft/registry/TofuEnchantments.java
@@ -108,7 +108,7 @@ public class TofuEnchantments {
 						)
 						.withEffect(
 								EnchantmentEffectComponents.DAMAGE,
-								new AddValue(LevelBasedValue.perLevel(0.5F)),
+								new AddValue(LevelBasedValue.perLevel(0.1F)),
 								LootItemEntityPropertyCondition.hasProperties(
 										LootContext.EntityTarget.DIRECT_ATTACKER, EntityPredicate.Builder.entity().of(TofuTags.EntityTypes.FUKUMAME).build()
 								)
