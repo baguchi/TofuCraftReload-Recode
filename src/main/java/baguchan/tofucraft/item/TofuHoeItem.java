6761e98cd91a4055046dff8816909f3bd9035a8f
baguchan <shutux500@gmail.com>
2024-07-07 22:04:06 +0900

    1.20.1-5.13.0.0

---
+++ src/main/java/baguchan/tofucraft/item/TofuHoeItem.java
@@ -0,0 +1,26 @@
+package baguchan.tofucraft.item;
+
+import baguchan.tofucraft.api.tfenergy.IEnergyInsertable;
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Tier;
+
+public class TofuHoeItem extends HoeItem implements IEnergyInsertable {
+	public TofuHoeItem(Tier tofuItemTier, Properties properties) {
+		super(tofuItemTier, properties);
+	}
+
+
+	@Override
+	public int fill(ItemStack inst, int energy, boolean simulate) {
+		int calculated = Math.min(energy, inst.getDamageValue());
+		if (!simulate) {
+			if (inst.getDamageValue() > 0) {
+				inst.setDamageValue(Mth.clamp(inst.getDamageValue() - calculated, 0, inst.getMaxDamage()));
+				return calculated * 5;
+			}
+		}
+		return 0;
+	}
+}
