44ce9b778d322ec56f7fc88a1400cd8ac7cff24d
baguchan <shutux500@gmail.com>
2024-08-18 07:24:11 +0900

    tag clean up

--- src/main/java/baguchan/tofucraft/data/generator/CustomTagGenerator.java
+++ src/main/java/baguchan/tofucraft/data/generator/CustomTagGenerator.java
@@ -8,6 +8,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.tags.PoiTypeTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.level.block.entity.BannerPattern;
@@ -49,7 +50,7 @@ public class CustomTagGenerator {
 		@Override
 		protected void addTags(HolderLookup.Provider p_256380_) {
 			tag(TofuTags.PoiTypes.TOFU_VILLAGE).add(TofuPoiTypes.TOFUNIAN_STATUE);
+			tag(PoiTypeTags.ACQUIRABLE_JOB_SITE).add(TofuPoiTypes.TOFU_CRAFTSMAN);
 		}
-
 	}
 }
