445146e2e055178c797bd9de73b84c532f2b9796
baguchan <shutux500@gmail.com>
2024-09-03 22:37:21 +0900

    oops

--- src/main/java/baguchan/tofucraft/registry/TofuCreativeModeTabs.java
+++ src/main/java/baguchan/tofucraft/registry/TofuCreativeModeTabs.java
@@ -465,7 +465,8 @@ public class TofuCreativeModeTabs {
 						TofuItems.LEEK_GREEN_CHEST_BOAT,
 						TofuItems.ZUNDAMA,
 						TofuItems.ZUNDARUBY,
-						TofuItems.TOFUGEM).map(itemRegistryObject -> {
+						TofuItems.TOFUGEM,
+						TofuItems.ADVANCE_TOFUGEM).map(itemRegistryObject -> {
 					return itemRegistryObject.get().getDefaultInstance();
 				}).toList()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
 			}).build());
