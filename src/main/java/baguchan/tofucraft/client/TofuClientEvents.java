d2a5dd34a9f201f14904bd4dc97be1e205b36f40
baguchan <shutux500@gmail.com>
2024-09-20 16:59:37 +0900

    1.20.1-5.15.0.0

--- src/main/java/baguchan/tofucraft/client/TofuClientEvents.java
+++ src/main/java/baguchan/tofucraft/client/TofuClientEvents.java
@@ -1,5 +1,6 @@
 package baguchan.tofucraft.client;
 
+
 import bagu_chan.bagus_lib.animation.BaguAnimationController;
 import bagu_chan.bagus_lib.api.client.IRootModel;
 import bagu_chan.bagus_lib.client.event.BagusModelEvent;
@@ -19,7 +20,7 @@ public class TofuClientEvents {
 	@SubscribeEvent
 	public static void onInitModelEvent(BagusModelEvent.Init event) {
 		IRootModel rootModel = event.getRootModel();
-		if (rootModel != null) {
+		if (event.isSupportedAnimateModel()) {
 			rootModel.getBagusRoot().getAllParts().forEach(ModelPart::resetPose);
 		}
 	}
@@ -28,7 +29,7 @@ public class TofuClientEvents {
 	public static void onAnimateModelEvent(BagusModelEvent.PostAnimate event) {
 		IRootModel rootModel = event.getRootModel();
 		BaguAnimationController controller = AnimationUtil.getAnimationController(event.getEntity());
-		if (rootModel != null && controller != null) {
+		if (event.isSupportedAnimateModel() && controller != null) {
 			rootModel.animateBagu(controller.getAnimationState(TofuAnimations.COUGH), CoughAnimation.COUGH, event.getAgeInTick());
 			if (event.getModel() instanceof HumanoidModel<?> humanoidModel) {
 				humanoidModel.hat.copyFrom(humanoidModel.head);
