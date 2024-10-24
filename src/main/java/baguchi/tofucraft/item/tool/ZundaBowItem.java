df46636b7b51820818ff7810ddf74d6c25e7f3a9
baguchan <shutux500@gmail.com>
2024-10-24 13:31:34 +0900

    add zundabow no longer break and lost

--- src/main/java/baguchi/tofucraft/item/tool/ZundaBowItem.java
+++ src/main/java/baguchi/tofucraft/item/tool/ZundaBowItem.java
@@ -3,7 +3,10 @@ package baguchi.tofucraft.item.tool;
 import baguchi.tofucraft.api.tfenergy.IEnergyInsertable;
 import baguchi.tofucraft.entity.projectile.ZundaArrow;
 import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
@@ -12,6 +15,24 @@ import net.minecraft.world.level.Level;
 public class ZundaBowItem extends BowItem implements IEnergyInsertable {
 	public ZundaBowItem(Properties tab) {
 		super(tab);
+	}
+
+	@Override
+	public boolean releaseUsing(ItemStack p_40667_, Level p_40668_, LivingEntity p_40669_, int p_40670_) {
+		if (!p_40667_.nextDamageWillBreak()) {
+			return super.releaseUsing(p_40667_, p_40668_, p_40669_, p_40670_);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public InteractionResult use(Level p_40672_, Player p_40673_, InteractionHand p_40674_) {
+		if (!p_40673_.getItemInHand(p_40674_).nextDamageWillBreak()) {
+			return super.use(p_40672_, p_40673_, p_40674_);
+		} else {
+			return InteractionResult.FAIL;
+		}
 	}
 
 	@Override
