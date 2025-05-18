bfebb0332561b9e2cb0b5674b2a1eb98dd5c70c8
baguchan <shutux500@gmail.com>
2025-05-18 19:12:49 +0900

    nerf boss attribute

--- src/main/java/baguchi/tofucraft/entity/ShuDofuSpider.java
+++ src/main/java/baguchi/tofucraft/entity/ShuDofuSpider.java
@@ -703,7 +703,7 @@ public class ShuDofuSpider extends Monster implements ISmartJump {
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
-		return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 500.0D).add(Attributes.FOLLOW_RANGE, 32F).add(Attributes.MOVEMENT_SPEED, 0.4D).add(Attributes.ATTACK_KNOCKBACK, 1.00F).add(Attributes.KNOCKBACK_RESISTANCE, 5.0D).add(Attributes.ARMOR, 14.0D).add(Attributes.ARMOR_TOUGHNESS, 1.0F).add(Attributes.ATTACK_DAMAGE, 19.0D);
+		return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 500.0D).add(Attributes.FOLLOW_RANGE, 32F).add(Attributes.MOVEMENT_SPEED, 0.4D).add(Attributes.ATTACK_KNOCKBACK, 1.00F).add(Attributes.KNOCKBACK_RESISTANCE, 5.0D).add(Attributes.ARMOR, 12.0D).add(Attributes.ARMOR_TOUGHNESS, 1.0F).add(Attributes.ATTACK_DAMAGE, 16.0D);
 	}
 
 	@Override
