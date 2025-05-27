eb538992de53855621d80e556496f097cc0f2133
baguchan <shutux500@gmail.com>
2025-05-27 18:33:52 +0900

    shudofu spider damage changed

--- src/main/java/baguchan/tofucraft/entity/ShuDofuSpider.java
+++ src/main/java/baguchan/tofucraft/entity/ShuDofuSpider.java
@@ -344,7 +344,7 @@ public class ShuDofuSpider extends Monster {
 								double d14 = Explosion.getSeenPercent(new Vec3(this.getX(), this.getY(), this.getZ()), entity);
 								double d10 = (1.0D - d12) * d14;
 								entity.addEffect(new MobEffectInstance(TofuEffects.COUGH, (int) (400 * d10), 0));
-								entity.hurt(this.damageSources().source(TofuDamageSource.SOY_SPORE, ShuDofuSpider.this), (float) (((d10 * d10 + d10) / 2.0D) * 26.0D));
+								entity.hurt(this.damageSources().source(TofuDamageSource.SOY_SPORE, ShuDofuSpider.this), (float) (((d10 * d10) / 2.0D) * 26.0D));
 							}
 						}
 					}
