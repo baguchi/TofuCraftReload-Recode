dd525d4d556c28c8e68e4d82fda725941ad7d22b
baguchan <shutux500@gmail.com>
2025-06-17 18:52:11 +0900

    fix tofu castle cannot break even defeat tofu gandlem

--- src/main/java/baguchi/tofucraft/world/TofuData.java
+++ src/main/java/baguchi/tofucraft/world/TofuData.java
@@ -14,13 +14,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class TofuData extends SavedData {
 	public static final Codec<TofuData> CODEC = RecordCodecBuilder.create(
 			p_400930_ -> p_400930_.group(
 							Codec.INT.fieldOf("traveler_spawn_delay").forGetter(p_400933_ -> p_400933_.travelerSpawnDelay),
 							Codec.FLOAT.fieldOf("traveler_spawn_chance").forGetter(p_400933_ -> p_400933_.travelerSpawnChance),
-							BoundingBox.CODEC.listOf().fieldOf("beated_bounding_box").forGetter(p_400933_ -> p_400933_.beatenDungeons)
+							BoundingBox.CODEC.listOf().fieldOf("beated_bounding_box").xmap(ArrayList::new, Function.identity()).forGetter(p_400933_ -> (ArrayList<BoundingBox>) p_400933_.beatenDungeons)
 					)
 					.apply(p_400930_, TofuData::new)
 	);
