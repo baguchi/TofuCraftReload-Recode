92ff82db5a0896b47d58721022f0db9f3ad8a486
baguchan <shutux500@gmail.com>
2025-08-23 04:38:23 +0900

    some soybean generation more rare

--- src/main/java/baguchi/tofucraft/world/gen/placement/ModNetherPlacements.java
+++ src/main/java/baguchi/tofucraft/world/gen/placement/ModNetherPlacements.java
@@ -32,10 +32,10 @@ public class ModNetherPlacements {
 
 	public static void bootstrap(BootstrapContext<PlacedFeature> context) {
 		HolderGetter<ConfiguredFeature<?, ?>> configuredFeature = context.lookup(Registries.CONFIGURED_FEATURE);
-		PlacementUtils.register(context, PATCH_NETHER_SOYBEAN_NORMAL, configuredFeature.getOrThrow(ModNetherFeatures.NETHER_SOYBEAN), List.of(RarityFilter.onAverageOnceEvery(3), InSquarePlacement.spread(), NETHER_SOYBEAN_HEIGHT, BiomeFilter.biome()));
+		PlacementUtils.register(context, PATCH_NETHER_SOYBEAN_NORMAL, configuredFeature.getOrThrow(ModNetherFeatures.NETHER_SOYBEAN), List.of(RarityFilter.onAverageOnceEvery(4), InSquarePlacement.spread(), NETHER_SOYBEAN_HEIGHT, BiomeFilter.biome()));
 
 		PlacementUtils.register(context, PATCH_NETHER_SOYBEAN_CRIMSON, configuredFeature.getOrThrow(ModNetherFeatures.NETHER_SOYBEAN), List.of(RarityFilter.onAverageOnceEvery(1), InSquarePlacement.spread(), NETHER_SOYBEAN_HEIGHT, BiomeFilter.biome()));
 
-		PlacementUtils.register(context, PATCH_SOUL_SOYBEAN, configuredFeature.getOrThrow(ModNetherFeatures.SOUL_SOYBEAN), List.of(RarityFilter.onAverageOnceEvery(4), InSquarePlacement.spread(), NETHER_SOYBEAN_HEIGHT, BiomeFilter.biome()));
+		PlacementUtils.register(context, PATCH_SOUL_SOYBEAN, configuredFeature.getOrThrow(ModNetherFeatures.SOUL_SOYBEAN), List.of(RarityFilter.onAverageOnceEvery(5), InSquarePlacement.spread(), NETHER_SOYBEAN_HEIGHT, BiomeFilter.biome()));
 	}
 }
