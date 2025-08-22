95057e0ceddeab92a3a365ede5a64a374e4497ee
baguchan <shutux500@gmail.com>
2025-08-23 05:16:32 +0900

    1.21.1-12.16.0.3 oops fixed rendering position wrong for mod itself

--- src/main/java/baguchan/tofucraft/client/render/blockentity/FoodPlateRender.java
+++ src/main/java/baguchan/tofucraft/client/render/blockentity/FoodPlateRender.java
@@ -93,16 +93,13 @@ public class FoodPlateRender implements BlockEntityRenderer<FoodPlateBlockEntity
 	}
 
 	public void renderBlock(PoseStack matrixStackIn, Direction direction, boolean candle) {
-		matrixStackIn.translate(0.0D, 0.05D, 0.0D);
 		float f = -direction.toYRot();
+		matrixStackIn.translate(0.5D, 0.0D, 0.5D);
 		if (!candle) {
-			matrixStackIn.translate(0.5D, 0.0D, 0.5D);
-
 			matrixStackIn.scale(0.6F, 0.6F, 0.6F);
-			matrixStackIn.translate(-0.5D, 0.0D, -0.5D);
 		}
-		matrixStackIn.translate(0.0D, 0.0D, 1.0D);
 		matrixStackIn.mulPose(Axis.YP.rotationDegrees(f));
+		matrixStackIn.translate(-0.5D, 0.0D, -0.5D);
 		//matrixStackIn.scale(0.8F, 0.8F, 0.8F);
 	}
 }
