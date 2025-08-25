d9272fe810f281193aac22fd609c907dd520284d
baguchan <shutux500@gmail.com>
2025-08-26 06:41:17 +0900

    1.21.8-18.2.0.0

--- src/main/java/baguchi/tofucraft/item/tool/ZundaBowItem.java
+++ src/main/java/baguchi/tofucraft/item/tool/ZundaBowItem.java
@@ -40,7 +40,8 @@ public class ZundaBowItem extends BowItem implements IEnergyInsertable {
 		Projectile projectile = super.createProjectile(p_331008_, p_330781_, p_330846_, p_331497_, p_331305_);
 
 		if (projectile instanceof ZundaArrow zundaArrow) {
-			zundaArrow.setBaseDamage(zundaArrow.baseDamage + 3);
+			zundaArrow.setBaseDamage(zundaArrow.baseDamage + 2);
+			return zundaArrow;
 		}
 
 		return projectile;
