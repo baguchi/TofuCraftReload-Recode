d39e1ca391a5953105dac8afc2391afa2ffd375d
baguchan <shutux500@gmail.com>
2025-08-26 13:58:04 +0900

    1.21.1-12.16.1.0 zunda bow extra damage reduced

--- src/main/java/baguchan/tofucraft/item/tool/ZundaBowItem.java
+++ src/main/java/baguchan/tofucraft/item/tool/ZundaBowItem.java
@@ -43,7 +43,7 @@ public class ZundaBowItem extends BowItem implements IEnergyInsertable {
 		Projectile projectile = super.createProjectile(p_331008_, p_330781_, p_330846_, p_331497_, p_331305_);
 
 		if (projectile instanceof ZundaArrow zundaArrow) {
-			zundaArrow.setBaseDamage(zundaArrow.getBaseDamage() + 3);
+			zundaArrow.setBaseDamage(zundaArrow.getBaseDamage() + 2);
 		}
 
 		return projectile;
