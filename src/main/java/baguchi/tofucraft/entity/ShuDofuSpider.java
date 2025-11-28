8779cc6ed77bfa6454baf7d02698a3bc41974f63
baguchan <shutux500@gmail.com>
2025-11-29 03:56:02 +0900

    Shudofu Spider now jump when anger is full and target is near

--- src/main/java/baguchi/tofucraft/entity/ShuDofuSpider.java
+++ src/main/java/baguchi/tofucraft/entity/ShuDofuSpider.java
@@ -298,6 +298,10 @@ public class ShuDofuSpider extends Monster implements ISmartJump {
 		}
 
 		if (!this.level().isClientSide()) {
+			if (this.isAlive() && this.rangedDamageReceived > 0.0F && this.tickCount % 80 == 0) {
+				this.rangedDamageReceived = Mth.clamp(this.rangedDamageReceived - 4.0F, 0, 60);
+			}
+
 			if (this.isAlive() && !this.isGraspAnim() && this.isAttackAnim() && this.getTarget() != null) {
 				++this.attackTime;
 				if (this.attackTime == 10) {
@@ -328,7 +332,12 @@ public class ShuDofuSpider extends Monster implements ISmartJump {
 				if (this.angerTime == 218) {
 					this.playSound(TofuSounds.TOFUSPIDER_AMBIENT.get(), 2.0F, (float) (0.45F + this.random.nextDouble() * 0.2F));
 				}
-				if (this.angerTime <= 200 && this.angerTime % 10 == 0) {
+				if (this.angerTime == 180 && this.distanceTo(this.getTarget()) < 16) {
+					ShuDofuSpider.this.setJump(true);
+					this.angerTime = 0;
+				}
+
+				if (this.angerTime <= 180 && this.angerTime % 10 == 0) {
 					if (this.random.nextInt(3) == 0) {
 						this.performBreathAttack(this.getTarget());
 						this.playSound(SoundEvents.ENDER_DRAGON_SHOOT, 2.0F, 0.4F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
