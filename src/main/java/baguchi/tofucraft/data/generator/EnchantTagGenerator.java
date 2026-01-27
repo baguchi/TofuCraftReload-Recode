170c27066f042dc1220fbbb0e7ddbd681c3fca4e
baguchan <shutux500@gmail.com>
2026-01-27 19:36:55 +0900

    1.21.11-21.0.9.0

--- src/main/java/baguchi/tofucraft/data/generator/EnchantTagGenerator.java
+++ src/main/java/baguchi/tofucraft/data/generator/EnchantTagGenerator.java
@@ -3,11 +3,9 @@ package baguchi.tofucraft.data.generator;
 import baguchi.tofucraft.TofuCraftReload;
 import baguchi.tofucraft.registry.TofuEnchantments;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EnchantmentTagsProvider;
 import net.minecraft.tags.EnchantmentTags;
-import net.minecraft.world.item.enchantment.Enchantment;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -18,20 +16,23 @@ public class EnchantTagGenerator extends EnchantmentTagsProvider {
 
 	@Override
 	protected void addTags(HolderLookup.Provider p_256380_) {
-		HolderLookup<Enchantment> lookup = p_256380_.lookupOrThrow(Registries.ENCHANTMENT);
 		this.tag(EnchantmentTags.TOOLTIP_ORDER).add(
-				lookup.get(TofuEnchantments.BATCH).get().key()
-				,
-				lookup.get(TofuEnchantments.DRAIN).get().key(),
-				lookup.get(TofuEnchantments.EFFECT_PROTECTION).get().key(),
-				lookup.get(TofuEnchantments.SHAPED_BEAN).get().key(),
-				lookup.get(TofuEnchantments.CRACK_BURST).get().key()
+				TofuEnchantments.BATCH,
+				TofuEnchantments.DRAIN,
+				TofuEnchantments.EFFECT_PROTECTION,
+				TofuEnchantments.SHAPED_BEAN,
+				TofuEnchantments.CRACK_BURST
 		);
+		this.tag(EnchantmentTags.NON_TREASURE).add(
+				TofuEnchantments.SHAPED_BEAN
+		);
+
 		this.tag(EnchantmentTags.TREASURE)
 				.add(
 						TofuEnchantments.BATCH,
 						TofuEnchantments.DRAIN,
-						TofuEnchantments.EFFECT_PROTECTION
+						TofuEnchantments.EFFECT_PROTECTION,
+						TofuEnchantments.CRACK_BURST
 				);
 	}
 }
