90bc7f43e182161421a734b0c519d565091a4bcd
baguchi <shutux500@gmail.com>
2026-04-29 23:28:15 +0900

    1.21.1-12.20.3.0 kouji base system changed and now stackable

--- src/main/java/baguchan/tofucraft/item/KoujiBaseItem.java
+++ src/main/java/baguchan/tofucraft/item/KoujiBaseItem.java
@@ -30,7 +30,7 @@ public class KoujiBaseItem extends Item {
 
 			//Prevent immediate syncing when items are added to the inventory.
 			if (stack.getPopTime() <= 0) {
-				if (level.getGameTime() % 20 == 0 && !stack.has(TofuDataComponents.FERMENTATION_DATA)) {
+				if (level.getGameTime() % 20 == 0 && (!stack.has(TofuDataComponents.FERMENTATION_DATA) || storedMinutes - 1 > minutes)) {
 					stack.set(TofuDataComponents.FERMENTATION_DATA, storedMinutes);
 				}
 			}
