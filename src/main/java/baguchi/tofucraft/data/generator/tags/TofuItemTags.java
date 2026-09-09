be81db8e74c53b1076984f7d021c0cf8fa786e81
baguchi <shutux500@gmail.com>
2026-09-09 13:00:05 +0900

    26.1.2-26.11.4.0

--- src/main/java/baguchi/tofucraft/data/generator/tags/TofuItemTags.java
+++ src/main/java/baguchi/tofucraft/data/generator/tags/TofuItemTags.java
@@ -41,6 +41,40 @@ public class TofuItemTags extends ItemTagsProvider {
 		tag(ItemTags.ARROWS).add(TofuItems.ZUNDA_ARROW.get());
 		tag(Tags.Items.SEEDS).add(TofuItems.SEEDS_CHILI.get(), TofuItems.SEEDS_SESAME.get(), TofuItems.SEEDS_SOYBEANS.get(), TofuItems.SEEDS_SOYBEANS_NETHER.get(), TofuItems.SEEDS_SOYBEANS_SOUL.get(), TofuItems.SEEDS_SOYBEANS_PALE.get(), TofuItems.SEEDS_SOYBEANS_PALE_GLOW.get(), TofuItems.SEEDS_RICE.get());
 
+		tag(Tags.Items.FOODS).addTag(TofuTags.Items.COOKED_TOFU).add(TofuItems.TOFU_ANKAKE.get()).add(TofuItems.SANBUZHAN.get()).add(TofuItems.CHIKUWA.get()).add(TofuItems.TOFU_CHIKUWA.get())
+				.add(TofuItems.TOFU_ISHI.get()).add(TofuItems.TOFU_STEAK.get()).add(TofuItems.OAGE.get())
+				.add(TofuBlocks.GRILLED_TOFU.asItem()).add(TofuBlocks.GIANT_OKARA_DONUT.asItem()).add(TofuItems.AGEDASHI_TOFU.get()).add(TofuItems.TOFU_ANNIN.get())
+				.add(TofuItems.SPROUTS.get()).add(TofuItems.NATTO.get()).add(TofuItems.NETHER_NATTO.get())
+				.add(TofuItems.YUBA.get()).add(TofuItems.BOILED_EDAMAME.get()).add(TofuItems.TOFU_HAMBURG.get())
+				.add(TofuItems.SOYMEAT.get()).add(TofuItems.RAW_TOFU_FISH.get()).add(TofuItems.COOKED_TOFU_FISH.get())
+				.add(TofuItems.MISODENGAKU.get()).add(TofuItems.MISO_CHEESE_DENGAKU.get()).add(TofuItems.TOFUCOOKIE.get()).add(TofuItems.TTTBURGER.get())
+				.add(TofuItems.MEAT_WRAPPED_YUBA.get()).add(TofuItems.SOYSTICK.get()).add(TofuItems.LONGER_SOYSTICK.get())
+				.add(TofuItems.MISOSOUP.get()).add(TofuItems.MOYASHIITAME.get()).add(TofuItems.MOYASHIOHITASHI.get())
+				.add(TofuItems.SALTYMELON.get()).add(TofuItems.CHILI.get()).add(TofuItems.MABODOFU.get()).add(TofuItems.NANBAN.get()).add(TofuItems.NANBANTOFU.get())
+				.add(TofuItems.FUKUMENI.get()).add(TofuItems.KOYADOFUSTEW.get()).add(TofuItems.KINAKO_MANJU.get()).add(TofuItems.ZUNDA_MANJU.get()).add(TofuItems.NETHER_MANJU.get()).add(TofuItems.SOUL_MANJU.get())
+				.add(TofuItems.SOY_CHOCOLATE.get()).add(TofuItems.TOFUNIAN_SOY_CHOCOLATE.get())
+				.add(TofuItems.ZUNDA_MOCHI.get()).add(TofuItems.KINAKO_MOCHI.get()).add(TofuItems.SESAME_OHAGI.get())
+				.add(TofuItems.CRIMSON_SOUP.get()).add(TofuItems.ONIGIRI.get()).add(TofuItems.ONIGIRI_SALT.get()).add(TofuItems.YAKIONIGIRI_MISO.get()).add(TofuItems.YAKIONIGIRI_SHOYU.get())
+				.add(TofuItems.RICE_BURGER.get()).add(TofuItems.RICE_NATTO.get()).add(TofuItems.RICE_NATTO_LEEK.get()).add(TofuItems.RICE_NETHER_NATTO.get()).add(TofuItems.RICE_NETHER_NATTO_LEEK.get())
+				.add(TofuItems.RICE_TOFU.get()).add(TofuItems.RICE_SOBORO_TOFU.get()).add(TofuItems.GOHEIMOCHI.get())
+				.add(TofuItems.INARI.get()).add(TofuItems.OKARASTICK.get()).add(TofuItems.OKARA_DONUT.get())
+				.add(TofuItems.SOBOROTOFUSAUTE.get()).add(TofuItems.PUDDING.get()).add(TofuItems.PUDDING_SOYMILK.get())
+				.add(TofuItems.NIKUJAGA.get()).add(TofuItems.APRICOT.get()).add(TofuItems.APRICOTJERRY_BREAD.get())
+				.add(TofuItems.TOMATO_SOYBEAN_STEW.get()).add(TofuItems.YUDOFU.get()).add(TofuItems.EDAMAME_RICE.get())
+				.add(TofuItems.TOFUSOMEN.get()).add(TofuItems.TASTYSTEW.get()).add(TofuItems.TASTYBEEFSTEW.get())
+				.add(TofuItems.HIYAYAKKO_GLASS.get()).add(TofuItems.NATTOHIYAYAKKO_GLASS.get())
+				.add(TofuItems.WARABI_MOCHI.get()).add(TofuItems.GRATIN.get()).add(TofuItems.CAPRESE.get())
+				.add(TofuItems.MUSHROOM_ANKAKE.get()).add(TofuItems.TOFU_ANKAKE.get())
+				.add(TofuItems.SOYSAUSE_RAMEN.get()).add(TofuItems.HELL_MABOU.get()).add(TofuItems.HELL_RED_SOUP.get()).add(TofuItems.RED_SOUP.get())
+				.add(TofuItems.SUKIYAKI.get()).add(TofuItems.TOFU_BUNS_BURGER.get())
+				.add(TofuItems.STEAMED_BREAD.get()).add(TofuItems.STEAMED_BREAD_COCOA.get()).add(TofuItems.STEAMED_BREAD_SESAME.get())
+				.add(TofuItems.SANBUZHAN.get()).add(TofuItems.SOY_CHEESE.get()).add(TofuItems.SOY_NETHER_CHEESE.get()).add(TofuItems.SOY_SOUL_CHEESE.get())
+				.add(TofuItems.KINAKO_BREAD.get()).add(TofuItems.EDAMAME_TEMPLA.get()).add(TofuItems.NEGIMA.get())
+				.add(TofuItems.SOY_KARAAGE.get()).add(TofuItems.SOYMEATDON.get())
+				.add(TofuItems.ZUNDA_INGOT.get())
+				.add(TofuItems.MONSTER_JERKY.get()).add(TofuItems.ROTTEN_PORK.get()).add(TofuItems.MONSTER_PORK_JERKY.get());
+
+
 		tag(TofuTags.Items.RICE_BLOCK).add(TofuBlocks.RICE_BLOCK.get().asItem());
 		tag(TofuTags.Items.SOYBEANS_BLOCK).add(TofuBlocks.SOYBEANS_SEEDS_BLOCK.get().asItem()).add(TofuBlocks.NETHER_SOYBEANS_SEEDS_BLOCK.get().asItem()).add(TofuBlocks.SOUL_SOYBEANS_SEEDS_BLOCK.get().asItem());
 		tag(Tags.Items.STORAGE_BLOCKS).addTags(TofuTags.Items.RICE_BLOCK).addTags(TofuTags.Items.SOYBEANS_BLOCK);
@@ -92,6 +126,11 @@ public class TofuItemTags extends ItemTagsProvider {
 		tag(TofuTags.Items.SOYSAUCE_SOYSAUCE).add(TofuItems.BOTTLE_SOYSAUSE.get());
 		tag(TofuTags.Items.SOYSAUCE).addTag(TofuTags.Items.SOYSAUCE_SOYSAUCE);
 		tag(TofuTags.Items.TOFU).add(TofuItems.TOFU_KINU.get()).add(TofuItems.TOFU_MOMEN.get());
+		tag(TofuTags.Items.COOKED_TOFU).add(TofuItems.TOFU_ISHI.get()).add(TofuItems.TOFU_ANNIN.get()).add(TofuItems.TOFU_SESAME.get())
+				.add(TofuItems.TOFU_FRIED.get()).add(TofuItems.TOFU_FRIED_POUCH.get())
+				.add(TofuItems.TOFU_ZUNDA.get()).add(TofuItems.TOFU_EGG.get()).add(TofuItems.TOFU_MISO.get()).add(TofuItems.TOFU_GRILLED.get()).add(TofuItems.TOFU_DRIED.get()).add(TofuItems.TOFU_STRAWBERRY.get()).add(TofuItems.TOFU_SMOKE.get())
+				.add(TofuItems.TOFU_HELL.get()).add(TofuItems.TOFU_SOUL.get()).add(TofuItems.SHUDOFU.get());
+
 		tag(TofuTags.Items.TOFU_FRIED).add(TofuItems.TOFU_FRIED.get());
 		tag(ItemTags.PIGLIN_REPELLENTS).add(TofuItems.SOUL_FUKUMAME.get(), TofuItems.SOUL_MANJU.get(), TofuItems.SEEDS_SOYBEANS_SOUL.get());
 		tag(ItemTags.TRIM_MATERIALS).add(TofuItems.ZUNDARUBY.get(), TofuItems.TOFU_METAL.get(), TofuItems.TOFU_DIAMOND.get());
