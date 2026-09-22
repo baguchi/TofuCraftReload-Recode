233af1c031396258c42d7a8dad26711483c0644b
baguchi <shutux500@gmail.com>
2026-09-22 10:54:39 +0900

    26.3-28.0.1.0 fixed tree grown by sapling

--- src/main/java/baguchi/tofucraft/world/gen/grower/TofuTreeGrowers.java
+++ src/main/java/baguchi/tofucraft/world/gen/grower/TofuTreeGrowers.java
@@ -3,6 +3,7 @@ package baguchi.tofucraft.world.gen.grower;
 import baguchi.tofucraft.TofuCraftReload;
 import baguchi.tofucraft.world.gen.features.ModTreeFeatures;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.random.Weighted;
 import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -10,25 +11,25 @@ import net.minecraft.world.level.levelgen.feature.Feature;
 public class TofuTreeGrowers {
 	public static final TreeGrower TOFU_TREE = new TreeGrower(
 			TofuCraftReload.prefix("tofu_tree").toString(),
+			WeightedList.of(new Weighted<>(ModTreeFeatures.TOFU_TREE, 9), new Weighted<>(ModTreeFeatures.TOFU_TREE_BIG, 1)),
+			WeightedList.of(),
 			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.TOFU_TREE).build(),
-			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.TOFU_TREE_BIG).build(),
-			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.TOFU_TREE).build(),
-			null
+			ModTreeFeatures.TOFU_TREE
 	);
 
 	public static final TreeGrower ZUNDA_MUSHROOM = new TreeGrower(
 			TofuCraftReload.prefix("zunda_mushroom").toString(),
+			WeightedList.of(new Weighted<>(ModTreeFeatures.ZUNDA_MUSHROOM, 9), new Weighted<>(ModTreeFeatures.ZUNDA_MUSHROOM_BIG, 1)),
+			WeightedList.of(),
 			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.ZUNDA_MUSHROOM).build(),
-			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.ZUNDA_MUSHROOM_BIG).build(),
-			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.ZUNDA_MUSHROOM).build(),
-			null
+			ModTreeFeatures.ZUNDA_MUSHROOM
 	);
 
 	public static final TreeGrower APRICOT_TREE = new TreeGrower(
 			TofuCraftReload.prefix("apricot_tree").toString(),
 			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.APRICOT_TREE).build(),
+			WeightedList.of(),
 			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.APRICOT_TREE).build(),
-			WeightedList.<ResourceKey<Feature>>builder().add(ModTreeFeatures.APRICOT_TREE).build(),
-			null
+			ModTreeFeatures.APRICOT_TREE
 	);
 }
